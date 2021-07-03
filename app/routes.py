25c2e5f4566a76a42cb0156e5c83048e8a4c40ed
ebkir <eb.kir@student.han.nl>
2021-07-04 00:20:25 +0200

    changes

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,15 @@ from app import app
 from app import db
 from flask import render_template, url_for, flash, redirect
 from app.models import Item
+from app import stockchecker
 
 @app.route("/")
 def index():
     print(Item.query.all())
     return "test ok"
+
+
+@app.route("/reload", methods=["GET", "POST"])
+def reload():
+    stockchecker.run()
+    return redirect("/")
